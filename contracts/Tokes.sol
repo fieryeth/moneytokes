c9e371de469355fce53f4a47b99736eef9e74d5f
Fiery <fieryeth@protonmail.com>
2022-02-18 23:49:07 +0100

    creator property in Toke to make breaking Tokes possible

--- contracts/Tokes.sol
+++ contracts/Tokes.sol
@@ -9,33 +9,53 @@ contract Tokes is ERC721 {
     using Counters for Counters.Counter;
     Counters.Counter private _tokesCounter;
 
+    // Amount of ether held in each Ether Toke
+    mapping(uint256 => Toke) private _tokes;
+
     struct Toke {
         uint256 value;
         uint256 expiration;
         address token;
+        address creator;
     }
 
-    // Amount of ether held in each Ether Toke
-    mapping(uint256 => Toke) private _tokes;
+    modifier onlyCreator(uint256 tokeId) {
+        require(msg.sender == _tokes[tokeId].creator, "You are not the creator of this toke.");
+        _;
+    }
 
     event MintEtherToke(address indexed from, address indexed to, uint256 indexed tokenId, uint256 amount, uint256 expiration);
-    event RedeemEtherToke(address indexed to, uint256 indexed tokenId, uint256 indexed amount);
+    event RedeemToke(address indexed to, uint256 indexed tokenId);
+    event BreakToke(address indexed creator, uint256 indexed tokenId);
 
     constructor() ERC721("Tokes", "Tokes") {}
 
-    function getToke(uint256 tokeId) public view returns (address owner, uint256 value, bool isEtherToke) {
+    function getToke(uint256 tokeId) public view returns(address owner, uint256 value, bool isEtherToke, uint256 expiration) {
         require(_exists(tokeId), "This toke does not exist.");
 
         if (_tokes[tokeId].token == address(0)) {
-            return (ownerOf(tokeId), _tokes[tokeId].value, true);
+            return (ownerOf(tokeId), _tokes[tokeId].value, true, _tokes[tokeId].expiration);
         }
 
+    }
+
+    function notExpired(uint256 tokeId) internal view returns(bool) {
+        uint256 expiration = _tokes[tokeId].expiration;
+        if(expiration == 0 || expiration > block.timestamp) {
+            return true;
+        }
+        return false;
     }
 
     function mintEtherToke(address to, uint256 expiration) public payable {
         require(msg.value > 0, "No ether sent");
 
-        _tokes[_tokesCounter.current()] = Toke({value: msg.value, expiration: expiration, token: address(0)});
+        _tokes[_tokesCounter.current()] = Toke({
+            value: msg.value,
+            expiration: expiration,
+            token: address(0),
+            creator: msg.sender
+        });
         _safeMint(to, _tokesCounter.current());
 
         emit MintEtherToke(msg.sender, to, _tokesCounter.current(), msg.value, expiration);
@@ -43,16 +63,28 @@ contract Tokes is ERC721 {
         _tokesCounter.increment();
     }
 
-    function redeem(uint256 tokeId) public {
-        require(_isApprovedOrOwner(msg.sender, tokeId), "You don't have the permissions to manage this toke.");
-
+    function _redeem(uint256 tokeId) internal {
+        // If this is an ether toke
         if (_tokes[tokeId].token == address(0)) {
             uint256 value = _tokes[tokeId].value;
             (bool sent, bytes memory data) = msg.sender.call{value: value}("");
             require(sent, "Failed to send ether.");
             _burn(tokeId);
             delete _tokes[tokeId];
-            emit RedeemEtherToke(msg.sender, tokeId, value);
+            emit RedeemToke(msg.sender, tokeId);
         }
+    }
+
+    function redeem(uint256 tokeId) public {
+        require(_isApprovedOrOwner(msg.sender, tokeId), "You don't have the permissions to manage this toke.");
+        require(notExpired(tokeId), "This toke expired.");
+
+        _redeem(tokeId);
+    }
+
+    function breakToke(uint256 tokeId) public onlyCreator(tokeId) {
+        require(!notExpired(tokeId), "This toke has not expired.");
+
+        _redeem(tokeId);
     }
 }
