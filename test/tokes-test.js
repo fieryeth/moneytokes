64584d510b8f043f2b4e9fd56a53b07cc414cf49
Fiery <fieryeth@protonmail.com>
2022-02-19 20:41:56 +0100

    test for mintERC20Toke

--- test/tokes-test.js
+++ test/tokes-test.js
@@ -37,4 +37,45 @@ describe("Tokes", () => {
     expect(parseFloat(ethers.utils.formatEther(await provider.getBalance(addr1.address)))).to.greaterThan(10009)
 
   })
+
+  it("Should deploy the Tokes contract and mint one ERC20 toke", async () => {
+    const [owner, addr1] = await ethers.getSigners();
+    const provider = owner.provider
+    const Tokes = await ethers.getContractFactory("Tokes");
+    const tokes = await Tokes.deploy()
+    await tokes.deployed();
+
+    const FakeToken = await ethers.getContractFactory("ERC20PresetFixedSupply");
+    const fakeToken = await FakeToken.deploy("FakeToken", "FT", 10000, owner.address)
+    await fakeToken.deployed();
+
+    // Checks that owner has 10k FT before minting
+    expect(await fakeToken.balanceOf(owner.address)).to.equal(10000);
+
+    // Mints a Toke with 10 ETH in it
+    await fakeToken.approve(tokes.address, 6000);
+    await tokes.mintERC20Toke(
+      fakeToken.address,
+      owner.address,
+      6000,
+      0,
+    );
+
+    // Checks that owner & Tokes contract have 4000 & 6000 FT after minting
+    expect(await fakeToken.balanceOf(owner.address)).to.equal(4000);
+    expect(await fakeToken.balanceOf(tokes.address)).to.equal(6000);
+
+    // Checks owner owns the Toke
+    expect(await tokes.ownerOf(0)).to.equal(owner.address);
+
+    // Checks addr1 owns the Toke
+    await tokes.transferFrom(owner.address, addr1.address, 0);
+    expect(await tokes.ownerOf(0)).to.equal(addr1.address);
+
+    // Checks addr1 redeemed the 6000 FT
+    expect(await fakeToken.balanceOf(addr1.address)).to.equal(0);
+    await tokes.connect(addr1).redeem(0);
+    expect(await fakeToken.balanceOf(addr1.address)).to.equal(6000);
+
+  })
 })
